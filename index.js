8e3610b9e1c4293065fa79863916008eef826b1b
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-05-12 21:42:06 +1000

    config: Install CORS

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+<<<<<<< Updated upstream
+=======
+const bodyParser = require("body-parser");
+const cors = require("cors");
+>>>>>>> Stashed changes
 require("dotenv").config();
 const database = require("./config/database");
 
 const app = express();
 const port = process.env.PORT;
 
+<<<<<<< Updated upstream
 // server APIs
+=======
+// enable CORS
+app.use(cors());
+
+// parse application/json
+app.use(bodyParser.json());
+
+// server APIs Version 1
+>>>>>>> Stashed changes
 const serverRoutes = require("./api/v1/routes/index.route");
 
 database.connect();
